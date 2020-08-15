8da31bfda8166b6a1e5d3229df04ba66b4bebf25
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2020-08-15 16:57:51 +0600

    Working for Brand
    1. Add some brand in database.
    2. Create LoadBrandViewComponent class in ViewComponents folder in GroceryShopApp project.
    3. Create Default view for LoadBrandViewComponent in shared -> Components -> LoadBrand folder in GroceryShopApp project.
    4. Add Component.InvokeAsync method for CustomerLayout view.

--- GroceryShopApp/GroceryShopApp/Startup.cs
+++ GroceryShopApp/GroceryShopApp/Startup.cs
@@ -41,6 +41,8 @@ namespace GroceryShopApp
 
             services.AddTransient<ICategoryManager, CategoryManager>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IBrandManager, BrandManager>();
+            services.AddTransient<IBrandRepository, BrandRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ GroceryShopApp/GroceryShopApp/ViewComponents/LoadBrandViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using GroceryShopApp.Manager.Contracts;
+using GroceryShopApp.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryShopApp.ViewComponents
+{
+    public class LoadBrandViewComponent : ViewComponent
+    {
+        private readonly IBrandManager _iBrandManager;
+
+        public LoadBrandViewComponent(IBrandManager iBrandManager)
+        {
+            _iBrandManager = iBrandManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ICollection<Brand> brands = _iBrandManager.GetAll()
+                .Where(b => b.Featured == true && b.Status == true).ToList();
+            return View(brands);
+        }
+    }
+}
